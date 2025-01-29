74fcc1e29747e9ec7aa96008e0a069808de4fd1e
Hennadii71 <genadisema@gmail.com>
2025-01-29 22:35:28 +0200

    Add .env

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import cors from "cors";
-
+import dotenv from "dotenv";
 import contactsRouter from "./routes/api/contacts-router.js";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
