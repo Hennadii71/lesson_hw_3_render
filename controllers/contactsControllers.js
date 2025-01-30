8b95a18da3e02bfe29f2236f6fac02803a7b63be
Hennadii71 <genadisema@gmail.com>
2025-01-30 23:30:49 +0200

    Create schema and model for mongo db

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,10 +1,4 @@
-import {
-  getAllContacts,
-  getContactsById,
-  addContact,
-  updateContactById,
-  deleteContactById,
-} from "../services/contactsServices.js";
+import Contact from "../models/Contact.js";
 import HttpError from "../helpers/HttpError.js";
 import {
   createContactSchema,
@@ -13,75 +7,75 @@ import {
 
 const getContacts = async (req, res, next) => {
   try {
-    const contacts = await getAllContacts();
+    const contacts = await Contact.find();
     res.json(contacts);
   } catch (error) {
     next(error);
   }
 };
 
-const getById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const contact = await getContactsById(id);
-    if (!contact) {
-      throw HttpError(404, `Contact with id=${id} Not Found`);
-    }
-    res.json(contact);
-  } catch (error) {
-    next(error);
-  }
-};
+// const getById = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const contact = await getContactsById(id);
+//     if (!contact) {
+//       throw HttpError(404, `Contact with id=${id} Not Found`);
+//     }
+//     res.json(contact);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const createContact = async (req, res, next) => {
-  try {
-    const { error } = createContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact);
-  } catch (error) {
-    next(error);
-  }
-};
+// const createContact = async (req, res, next) => {
+//   try {
+//     const { error } = createContactSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const newContact = await addContact(req.body);
+//     res.status(201).json(newContact);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const updateContact = async (req, res, next) => {
-  try {
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
-    const { id } = req.params;
-    const updatedContact = await updateContactById(id, req.body);
-    if (!updatedContact) {
-      throw HttpError(404, `Contact with id=${id} Not Found`);
-    }
-    res.json(updatedContact);
-  } catch (error) {
-    next(error);
-  }
-};
+// const updateContact = async (req, res, next) => {
+//   try {
+//     const { error } = updateContactSchema.validate(req.body);
+//     if (error) {
+//       throw HttpError(400, error.message);
+//     }
+//     const { id } = req.params;
+//     const updatedContact = await updateContactById(id, req.body);
+//     if (!updatedContact) {
+//       throw HttpError(404, `Contact with id=${id} Not Found`);
+//     }
+//     res.json(updatedContact);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
-const deleteContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const deletedContact = await deleteContactById(id);
-    if (!deletedContact) {
-      throw HttpError(404, `Contact with id=${id} Not Found`);
-    }
-    res.status(200).json(deletedContact);
-    // res.status(204).send();
-    // res.json({message: "Contact deleted"});
-  } catch (error) {
-    next(error);
-  }
-};
+// const deleteContact = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const deletedContact = await deleteContactById(id);
+//     if (!deletedContact) {
+//       throw HttpError(404, `Contact with id=${id} Not Found`);
+//     }
+//     res.status(200).json(deletedContact);
+//     // res.status(204).send();
+//     // res.json({message: "Contact deleted"});
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 export default {
   getContacts,
-  getById,
-  createContact,
-  updateContact,
-  deleteContact,
+  // getById,
+  // createContact,
+  // updateContact,
+  // deleteContact,
 };
