8ad8727e45975f924c960459f5077e3001b67a63
Hennadii71 <genadisema@gmail.com>
2025-01-30 23:34:35 +0200

    Coment routes

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -5,16 +5,16 @@ const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getContacts);
 
-contactsRouter.get("/:id", contactsControllers.getById);
+// contactsRouter.get("/:id", contactsControllers.getById);
 
-contactsRouter.post("/", isEmptyBody, contactsControllers.createContact);
+// contactsRouter.post("/", isEmptyBody, contactsControllers.createContact);
 
-contactsRouter.put("/:id", isEmptyBody, contactsControllers.updateContact);
+// contactsRouter.put("/:id", isEmptyBody, contactsControllers.updateContact);
 
-contactsRouter.delete("/:id", contactsControllers.deleteContact);
+// contactsRouter.delete("/:id", contactsControllers.deleteContact);
 
-contactsRouter.delete("/:id", (req, res) => {
-  res.json(contacts[0]);
-});
+// contactsRouter.delete("/:id", (req, res) => {
+//   res.json(contacts[0]);
+// });
 
 export default contactsRouter;
